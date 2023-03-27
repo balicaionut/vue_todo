39280e5f30abd27129bb8ef255e12b969f6f48a7
Ionut Balica <balicaionut@gmail.com>
2023-03-27 15:01:36 +0300

    added basic functionalities
    -dynamically display tasks
    -adding task
    -saving to Local Storage
    -deleting all tasks-minor tweaks to original css

--- src/components/todoMain.vue
+++ src/components/todoMain.vue
@@ -3,20 +3,30 @@
         <main id="app">
             <section class="todo-list">
                 <h3>To-do List</h3>
-                <div class="all-todos">
-                    <div class="single-todo done">
-                        <p>Learn HTML, CSS and Javascript</p>
+                <div v-if="todoList" class="all-todos">
+                    <div
+                        v-for="todo in todoList"
+                        :key="todo.text"
+                        class="single-todo"
+                        :class="{ done: todo.done }"
+                    >
+                        <p>{{ todo.text }}</p>
                     </div>
-                    <div class="single-todo done">
-                        <p>Learn the basics of Vue JS</p>
-                    </div>
-                    <div class="single-todo">
-                        <p>Complete Vue JS Challenge with excellence</p>
-                    </div>
+                    <button class="clear" @click="clearAllTodos">Clear All</button>
                 </div>
-                <button class="clear">Clear All</button>
-                <label for="todoAdd">Add new todo <input type="text" id="todoAdd"></label>
-                <button class="add">Add</button>
+                <div v-else class="all-todos">Nothing todo</div>
+
+                <label for="todoAdd">
+                    <input
+                        type="text"
+                        placeholder="Add new todo"
+                        id="todoAdd"
+                        v-model="newTodo"
+                        ref="todoAdd"
+                    >
+                </label>
+                <button v-if="newTodo" class="add-active" @click="addTodo">Add</button>
+                <button v-else class="add-inactive">Add</button>
                 <div class="instructions">
                     Instructions:
                     <ul>
@@ -31,8 +41,40 @@
 </template>
 
 <script>
-export default {
 
+export default {
+  name: 'todoMain',
+  components: {
+    // TODO create todo item componeent
+  },
+  data() {
+    return {
+      newTodo: '',
+      todoList: [],
+    };
+  },
+  computed: {
+    loadTodoList() {
+      return JSON.parse(window.localStorage.getItem('todoList'));
+    },
+  },
+  mounted() {
+    this.todoList = this.loadTodoList ? this.loadTodoList : [];
+  },
+  methods: {
+    addTodo() {
+      const todo = {
+        text: this.$refs.todoAdd.value,
+        done: false,
+      };
+      this.todoList.push(todo);
+      localStorage.setItem('todoList', JSON.stringify(this.todoList));
+    },
+    clearAllTodos() {
+      this.todoList = [];
+      localStorage.setItem('todoList', JSON.stringify(this.todoList));
+    },
+  },
 };
 </script>
 
